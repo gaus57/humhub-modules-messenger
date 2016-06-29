f337ed6505325297087e2cffe8aae0922c52bd22
Олег Кузнецов < >
2016-06-30 00:42:32 +0500

    register assets fix

--- Assets.php
+++ Assets.php
@@ -7,16 +7,21 @@ use Yii;
 
 class Assets extends AssetBundle
 {
+    public $publishOptions = [
+        'forceCopy' => true
+    ];
 
     public $css = [
         'chat.css',
     ];
+    
     public $js = [
         'chat.js',
     ];
 
     public function init()
     {
+        $this->sourcePath = dirname(__FILE__) . '/assets';
         $params = require(__DIR__ . '/params.php');
         $this->js[] = Yii::$app->request->hostInfo . ':' . $params['node_server_port'] . '/socket.io/socket.io.js';
         $this->sourcePath = __DIR__ . '/assets';
